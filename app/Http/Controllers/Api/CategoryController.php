24095b4d6229cc0511803a7e1d51d2413431a369
vitorgomes7001 <vitorgomes7001@gmail.com>
2021-03-02 19:03:14 +0000

    fix: remoção de métodos não necessário e movendo o arquivo

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategoryController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(Category $category)
+    {
+        //
+    }
+
+
+    public function update(Request $request, Category $category)
+    {
+        //
+    }
+
+
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
