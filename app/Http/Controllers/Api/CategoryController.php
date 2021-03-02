080440f70e9ab557ab1ed6848308cb234ca053b0
vitorgomes7001 <vitorgomes7001@gmail.com>
2021-03-02 19:15:52 +0000

    feat: retornar todos os registro e apenas um registro

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -11,7 +11,7 @@ class CategoryController extends Controller
 
     public function index()
     {
-        //
+        return Category::all();
     }
 
 
@@ -23,7 +23,7 @@ class CategoryController extends Controller
 
     public function show(Category $category)
     {
-        //
+        return $category;
     }
 
 
