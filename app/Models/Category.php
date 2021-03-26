0d73038fe1a1e19f5a6c64bb24867e0a5ab294dd
vitorgomes7001 <vitorgomes7001@gmail.com>
2021-03-26 16:28:46 -0300

    feat: usando uuids para Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Ramsey\Uuid\Uuid;
 
 class Category extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, \App\Models\Traits\Uuid;
+
     protected $fillable = ['name','description', 'is_active'];
     protected $dates = ['deletec_at'];
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+
 }
