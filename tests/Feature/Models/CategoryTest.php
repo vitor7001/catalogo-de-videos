48c56ee4b3fe108603252f998500ae8c3c821199
vitorgomes7001 <vitorgomes7001@gmail.com>
2021-03-31 15:16:17 +0000

    feat: teste criação de Category

--- tests/Feature/Models/CategoryTest.php
+++ tests/Feature/Models/CategoryTest.php
@@ -16,12 +16,50 @@ class CategoryTest extends TestCase
     public function testList()
     {
 
-        factory(Category::class,1)->create();
+        factory(Category::class, 1)->create();
         $categories = Category::all();
         $this->assertCount(1, $categories);
         $categoryKeys = array_keys($categories->first()->getAttributes());
         $this->assertEqualsCanonicalizing([
             'id', 'name', 'description', 'is_active', 'created_at', 'updated_at', 'deleted_at'
         ], $categoryKeys);
+    }
+
+    public function testCreate()
+    {
+        $category = Category::create([
+            'name' => 'teste'
+        ]);
+
+        $category->refresh();
+        $this->assertEquals('teste', $category->name);
+        $this->assertNull($category->description);
+        $this->assertTrue($category->is_active);
+
+
+        $category = Category::create([
+            'name' => 'teste',
+            'description' => null
+        ]);
+        $this->assertNull($category->description);
+
+
+        $category = Category::create([
+            'name' => 'teste',
+            'description' => 'teste_description'
+        ]);
+        $this->assertEquals('teste_description',$category->description);
+
+        $category = Category::create([
+            'name' => 'teste',
+            'is_active' => false
+        ]);
+        $this->assertFalse($category->is_active);
+
+        $category = Category::create([
+            'name' => 'teste',
+            'is_active' => true
+        ]);
+        $this->assertTrue($category->is_active);
     }
 }
