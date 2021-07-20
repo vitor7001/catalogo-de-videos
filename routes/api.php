816eb03dfbdf0f411f749965910e9685d5aa47aa
vitorgomes7001 <vitorgomes7001@gmail.com>
2021-07-20 22:25:49 +0000

    feat: teste video controller

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,6 @@ Route::group(['namespace' => 'Api'], function(){
     Route::resource('categories', 'CategoryController', $exceptCreateAndEdit);
     Route::resource('genres', 'GenreController', $exceptCreateAndEdit);
     Route::resource('cast_members', 'CastMemberController', $exceptCreateAndEdit);
-    Route::resource('videos', 'VideosController', $exceptCreateAndEdit);
+    Route::resource('videos', 'VideoController', $exceptCreateAndEdit);
 
 });
---
+++ tests/Feature/Http/Controllers/Api/VideoControllerTest.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use App\Models\Video;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use Tests\Traits\TestSaves;
+use Tests\Traits\TestValidations;
+
+class VideoControllerTest extends TestCase
+{
+    use DatabaseMigrations, TestValidations, TestSaves;
+
+    private $video;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->video = factory(Video::class)->create();
+    }
+
+    public function testIndex()
+    {
+        $response = $this->get(route('videos.index'));
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([$this->video->toArray()]);
+    }
+
+    public function testShow()
+    {
+        $response = $this->get(route('videos.show', ['video' => $this->video->id]));
+        $response
+            ->assertStatus(200)
+            ->assertJson($this->video->toArray());
+    }
+
+
+    public function testInvalidationRequired()
+    {
+        $data = [
+            'title' => '',
+            'description' => '',
+            'year_launched' => '',
+            'rating' => '',
+            'duration' => ''
+        ];
+        $this->assertInvalidationInStoreAction($data, 'required');
+        $this->assertInvalidationInUpdateAction($data, 'required');
+    }
+
+    public function testInvalidationMax()
+    {
+        $data = [
+            'title' => str_repeat('a', 256)
+        ];
+        $this->assertInvalidationInStoreAction($data, 'max.string', ['max' => 255]);
+        $this->assertInvalidationInUpdateAction($data, 'max.string', ['max' => 255]);
+    }
+
+    public function testInvalidationDurationField()
+    {
+        $data = [
+            'duration' => 'a'
+        ];
+        $this->assertInvalidationInStoreAction($data, 'integer');
+        $this->assertInvalidationInUpdateAction($data, 'integer');
+    }
+
+    public function testInvalidationYearLauchedField()
+    {
+        $data = [
+            'year_launched' => 'a'
+        ];
+        $this->assertInvalidationInStoreAction($data, 'date_format', ['format' => 'Y']);
+        $this->assertInvalidationInUpdateAction($data, 'date_format', ['format' => 'Y']);
+    }
+
+    public function testInvalidationOpenedField()
+    {
+        $data = [
+            'opened' => 'a'
+        ];
+        $this->assertInvalidationInStoreAction($data, 'boolean');
+        $this->assertInvalidationInUpdateAction($data, 'boolean');
+    }
+
+    public function testInvalidationRatingField()
+    {
+        $data = [
+            'rating' => 0
+        ];
+        $this->assertInvalidationInStoreAction($data, 'in');
+        $this->assertInvalidationInUpdateAction($data, 'in');
+    }
+
+
+    protected function routeStore()
+    {
+        return route('videos.store');
+    }
+
+    protected function routeUpdate()
+    {
+        return route('videos.update', ['video' => $this->video->id]);
+    }
+
+    protected function model()
+    {
+        return Video::class;
+    }
+}
